eea0086bc4feb0e9a2314b57fdf7fa4287432ca7
Martine <martine.moses@gmail.com>
2022-08-30 18:32:47 +0000

    Pending changes exported from your codespace

--- src/backend/backend.go
+++ src/backend/backend.go
@@ -1,17 +1,41 @@
 package backend
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
 )
+
+type App struct {
+	DB     *sql.DB
+	Port   string
+	Router *mux.Router
+}
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World\n")
 }
 
-func Run(addr string) {
-	http.HandleFunc("/", helloWorld)
-	fmt.Println("Server started and listening on port ", addr)
-	log.Fatal(http.ListenAndServe(addr, nil))
+func (a *App) Initialize() {
+	DB, err := sql.Open("sqlite3", "../../practiceit.db")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	a.DB = DB
+	a.Router = mux.NewRouter()
+	a.initalizeRoutes()
+}
+
+func (a *App) initalizeRoutes() {
+	a.Router.HandleFunc("/", helloWorld)
+}
+
+func (a *App) Run() {
+	fmt.Println("Server started and listening on port ", a.Port)
+	log.Fatal(http.ListenAndServe(a.Port, a.Router))
 }
--- src/backend/go.mod
+++ src/backend/go.mod
@@ -1,3 +1,8 @@
 module example.com/backend
 
 go 1.18
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/mattn/go-sqlite3 v1.14.15
+)
